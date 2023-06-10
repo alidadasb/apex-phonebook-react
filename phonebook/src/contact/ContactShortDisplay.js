24930fef664eaa9d7224491fc9740fa80303b7e5
vireddy87 <vireddy87@gmail.com>
2023-06-10 10:57:12 -0400

    React to ts conversion

--- phonebook/src/contact/ContactShortDisplay.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const ContactShortDisplay = (props) => {
-    let item = props.contact;
-
-    return <span >{item.firstName}, {item.lastName} ,{item.gender} ,{item.email}</span>
-}
